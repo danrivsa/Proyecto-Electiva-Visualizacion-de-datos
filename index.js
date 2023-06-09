2280110670d138f94c128a5c760a1852b586afd4
Daniel Rivero <danielrivero2000@gmail.com>
2023-06-09 01:09:15 -0400

    [feat] agrega endpoint y formateo de datos para consultas de datamart equipo 5

--- index.js
+++ index.js
@@ -9,15 +9,86 @@ app.get('/', (req, res) => {
     res.send('¡Hola, mundo desde equipo 5!');
   });
 
-app.get('/sucursal',async (req,res)=>{
+app.get('/datamart-equipo-5/obtener-datos-dw',async (req,res)=>{
   try{
-    const dw_res = await dw.query('SELECT * FROM sucursal');
-    res.send(dw_res.rows); 
+    //preparar envio de data
+    //datos de la sucursal
+    const res_sucursal = await dw.query('SELECT * FROM sucursal where id = 1');
+    const data_sucursal = res_sucursal.rows.map((res)=>{
+      return {
+        id_sucursal: res.id,
+        nombre: res.nombre,
+        ubicacion: res.ubicacion,
+        longitud: res.longitud,
+        latitud: res.latitud,
+        fecha_carga: res.fecha_carga
+
+      }
+    })[0];
+
+    //inventario vehiculos
+    const res_vehiculos = await dw.query(`SELECT * FROM vehiculo`);
+    const inventario_vehiculos = res_vehiculos.rows.map((res)=>{
+      return{
+        id_modelo: res.id,
+        marca: res.marca,
+        anio_fabricacion: new Date(res.fecha_fabricacion).getFullYear(),
+        nombre_modelo: res.modelo,
+        fecha_carga: res.fecha_carga
+      }
+    });
+
+    //metodo de pago
+    const res_metodo_pago = await dw.query(`SELECT * FROM metodo_pago`);
+    const metodos_de_pago = res_metodo_pago.rows.map((res)=>{
+      return {
+        id_metodo: res.id,
+        nombre:res.nombre,
+        fecha_carga:res.fecha_carga
+      }
+    });
+
+    const res_estadisticas_alquiler = await dw.query(`SELECT h.*,t.* FROM hechos_estadisticas_alquiler h INNER JOIN tiempo t on t.id = h.id_tiempo`);
+    const hechos_estadisticas_alquiler = res_estadisticas_alquiler.rows.map((res)=>{
+      return {
+        tiempo:{
+          id_anio: res.year,
+          id_mes: res.month,
+          id_dia: res.day,
+          id_hora: res.hour
+        },
+        porc_satisfaccion_cliente: res.porc_satisfaccion_cliente,
+        top_metodo_pago_utilizado: res.top_metodo_pago,
+        top_metodo_pago_porcentaje_util : 0,
+        top_modelo_vehiculo_alquilado : res.top_vehiculo_alquilado,
+        modelo_veces_alquilado : 0,
+        modelo_porcentaje_alquilado:0,
+        ingresos:res.ingresos,
+        egresos:res.egresos,
+        porc_ingresos_egresos:res.porc_ingresos_egresos,
+        balance_general:res.balance_general,
+        porc_vehiculos_disponibles:20,
+        duracion_promedio_alquiler:res.duracion_promedio_alquiler,
+        total_alquileres:res.total_alquileres,
+        porC_VEHICULOS_UTILIZADOS: 0,
+        modelO_VEHICULO_ID_MODELO: 0,
+        metodO_PAGO_ID_METODO: 0
+
+      }
+    });
+    const res_data = {
+      sucursal: data_sucursal,
+      modelo_vehiculos: inventario_vehiculos,
+      metodo_pagos:metodos_de_pago,
+      estadisticas_sucursal:hechos_estadisticas_alquiler
+    }
+    res.status(200).send(res_data);
   }catch(err){
-    res.status(500).send('error al pedir sucursal', err);
+    console.log(err);
+    res.status(500).send('Ocurrio un error inesperado', err);
   }
 });
 
 app.listen(port,'0.0.0.0' ,() => {
-  console.log(`Servidor iniciado en el puerto ${process.env.APP_PORT}`);
+  console.log(`Servidor iniciado en el puerto ${process.env.PORT}`);
 });
